d619b7df13e3f8e8c0a92f9c57865f117fccc3ea
Marek Hückmann <marek.hueckmann@gmail.com>
2017-08-25 10:46:50 +0200

    Update portfolio.js
    adding IIFE

--- portfolio.js
+++ portfolio.js
@@ -2,3 +2,14 @@ $('#fullpage').fullpage({
   anchors: ['main', 'about', 'projects', 'contact'],
   sectionsColor: ['white']
 });
+
+var circles = document.querySelectorAll(".circle");
+
+(function() {
+	for(var i=0; i<circles.length; i++) {
+		circles[i].addEventListener("click", function() {
+			var clickedColor = this.style.background;
+				this.style.color = "white";
+		});
+	}
+}());
