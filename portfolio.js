b8c8094892682c7a8eaf16cb9a228e8336f306cb
Marek Hückmann <marek.hueckmann@gmail.com>
2017-08-25 02:43:04 +0200

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,9 +2,3 @@ $('#fullpage').fullpage({
   anchors: ['main', 'about', 'projects', 'contact'],
   sectionsColor: ['white']
 });
-
-var x = document.getElementById("display").offsetHeight;
-x*=2;
-
-var marginSetter = document.querySelector("#container");
-marginSetter.style.margin =  x + "px auto";
