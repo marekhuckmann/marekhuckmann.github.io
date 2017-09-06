63e575c28a02f294d789540a72783a43074b71e4
Marek Hückmann <marek.hueckmann@gmail.com>
2017-09-06 15:42:39 +0200

    HTMLPrettify

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,15 @@
 $('#fullpage').fullpage({
-  anchors: ['main', 'about', 'projects', 'contact'],
-  sectionsColor: ['white']
+    anchors: ['main', 'about', 'projects', 'contact'],
+    sectionsColor: ['white']
 });
 
 var circles = document.querySelectorAll(".circle");
 
 (function() {
-	for(var i=0; i<circles.length; i++) {
-		circles[i].addEventListener("click", function() {
-			var clickedColor = this.style.background;
-      this.style.color = "white";
-    });
-	}
+    for (var i = 0; i < circles.length; i++) {
+        circles[i].addEventListener("click", function() {
+            var clickedColor = this.style.background;
+            this.style.color = "white";
+        });
+    }
 }());
